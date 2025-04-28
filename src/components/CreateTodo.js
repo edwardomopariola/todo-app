d5a9c9b35a83444f3b6dc6f9d5dd30b52d1e293f
Edward Omopariola <femiedjr@gmail.com>
2025-04-28 18:01:12 -0400

    delete and edit function completed

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';  // Importing useState hook from React
 
-const CreateTodo = ({ IsSignedin}) => {  // Component to create a new todo
+const CreateTodo = ({ isSignedIn}) => {  // Component to create a new todo
     const [ todos, setTodos ] = useState([]);  // State to store the list of todos
     const [ newTodo, setNewTodo ] = useState("");  // State to store the new todo input value
     const [ editTodos, setEditTodos ] = useState(null);  // State to store the value of the todo being edited
@@ -14,52 +14,60 @@ const CreateTodo = ({ IsSignedin}) => {  // Component to create a new todo
     };
 
     const iseditTodos = (index) => {  // Function to edit an existing todo
-        if (IsSignedin) {  // Check if the user is signed in
+        if (isSignedIn) {  // Check if the user is signed in
             setEditIndex(index);  // Set the index of the todo being edited
             setEditTodos(todos[index]);  // Set the current todo value in the input field
         } 
     };
 
     const deleteTodo = (index) => {  // Function to delete a todo
-        if (IsSignedin) {  // Check if the user is signed in
+        if (isSignedIn) {  // Check if the user is signed in
             const updatedTodos = todos.filter((_, i) => i !== index);  // Filter out the todo at the specified index
             setTodos(updatedTodos);  // Set the updated list of todos
         }
     }
 
     const saveEdit = () => {  // Function to save the edited todo
-        if (IsSignedin && editIndex.trim()) {  // Check if the user is signed in and the edit index is not empty
+        if (isSignedIn && editTodos.trim()) {  // Check if the user is signed in and the edit index is not empty
             const updatedTodos = [...todos];  // Create a copy of the current list of todos
             updatedTodos[editIndex] = editTodos;  // Update the todo at the specified index
             setTodos(updatedTodos);  // Set the updated list of todos
             setEditIndex(null);  // Clear the edit index
-            setEditText('');  // Clear the input field
+            setEditTodos('');  // Clear the input field
         }
     }
 
     return (
-        <div>
-            <h2>Create Todo</h2> 
-            <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} placeholder="Add a new todo" /> 
-            <button onClick={addTodo}>Add Todo</button>  
-            <ul>  
+        <div className='todo-container'> 
+            <h2 className='todo-header'>Create Todo</h2> 
+            <div className='todo-input'> 
+                <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} placeholder="Add a new todo" className='input-field'/> 
+                <button onClick={addTodo}>Add Todo</button>  
+            </div>
+            <ul className='todo-list'>  
                 {todos.map((todo, index) => (  // Map through the list of todos and display them
-                    <li key={index}>{todo} 
+                    <li key={index} className='todo-item'>
                         {editIndex === index ? (  // Check if the todo is being edited
-                            <div>
-                                <input type="text" value={editTodos} onChange={(e) => setEditTodos(e.target.value)} /> 
-                                <button onClick={saveEdit}>Save</button>  {/* Save the edited todo */}
-                            </div>
-                        ) : (  // If not editing, show the edit button
-                            <span>{todo}</span>
+                            <>
+                                <input type="text" value={editTodos} onChange={(e) => setEditTodos(e.target.value)} className='edit-input'/> 
+                                <button onClick={saveEdit} className='save-edit'>Save</button>  {/* Save the edited todo */}
+                            </>
+                        ) : ( 
+                            <>
+                                <span>{todo}</span>
+                                {isSignedIn &&  (
+                                    <>
+                                        <button className="edit-button" onClick={() => iseditTodos(index)}>Edit</button>  
+                                        <button className="delete-button" onClick={() => deleteTodo(index)}>Delete</button>  
+                                    </>
+                                )}
+                            </>
                         )}
                     </li>
                 ))}
             </ul>
         </div>
-    );
-
-    
-}
+    );  // Return the JSX for the component
+};
 
 export default CreateTodo;  // Export the CreateTodo component
