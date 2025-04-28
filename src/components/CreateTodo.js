8f459f087aca8944361b0606570d28f04ee4e661
Edward Omopariola <femiedjr@gmail.com>
2025-04-28 17:06:50 -0400

    setting my edit function

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,14 +1,22 @@
 import { useState } from 'react';  // Importing useState hook from React
 
-const CreateTodo = ({}) => {  // Component to create a new todo
+const CreateTodo = ({ IsSignedin}) => {  // Component to create a new todo
     const [ todos, setTodos ] = useState([]);  // State to store the list of todos
     const [ newTodo, setNewTodo ] = useState("");  // State to store the new todo input value
+    const [ editTodos, setEditTodos ] = useState(false);  // State to handle editing of todos
+    const [ editIndex, setEditIndex ] = useState(null);  // State to store the index of the todo being edited
     
     const addTodo = () => {  // Function to add a new todo}
         if (newTodo) {  // Check if the new todo is not empty
             setTodos([ ...todos, newTodo ]);  // Add the new todo to the list of todos
             setNewTodo('');  // Clear the input field
         }
+    };
+
+    const iseditTodos = (index) => {  // Function to edit an existing todo
+        const updatedTodos = todos.map((todo, i) => (i === index ? newTodo : todo));  // Update the todo at the specified index
+        setTodos(updatedTodos);  // Set the updated list of todos
+        setNewTodo('');  // Clear the input field
     };
 
     return (
@@ -18,7 +26,19 @@ const CreateTodo = ({}) => {  // Component to create a new todo
             <button onClick={addTodo}>Add Todo</button>  
             <ul>  
                 {todos.map((todo, index) => (  // Map through the list of todos and display them
-                    <li key={index}>{todo}</li>  // Display each todo in a list item
+                    <li key={index}>{todo} 
+                        {editIndex === index ? (  // Check if the todo is being edited
+                            <div>
+                                <input type="text" value={editTodos} onChange={(e) => setEditTodos(e.target.value)} /> 
+                                <button onClick={saveEdit}>Save</button>  {/* Save the edited todo */}
+                            </div>
+                        ) : (  // If not editing, show the edit button
+                            <button onClick={() => {  // Function to handle editing of the todo
+                                setEditIndex(index);  // Set the index of the todo being edited
+                                setEditTodos(todo);  // Set the current todo value in the input field
+                            }}>Edit</button>
+                        )}
+                    </li>
                 ))}
             </ul>
         </div>
